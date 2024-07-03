40d845c604176bc7d40e0c958d74e3ffde11fe8c
sneha-tiwari06 <tiwarisneha491@gmail.com>
2024-07-03 17:05:36 +0530

    final

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+require('dotenv').config();
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -29,38 +30,6 @@ const extractUTMParams = (req, res, next) => {
 
     next();
 };
-app.post('/api/contact', extractUTMParams, async (req, res) => {
-    const { name, time, phone, email, needs } = req.body;
-    const { utm_source, utm_medium, utm_campaign, utm_term, utm_content } = req;
-
-    const transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-            user: 'tiwarisneha491@gmail.com',
-            pass: 'qwpx wgzt yhok gqbq',
-        },
-    });
-
-    const mailOptions = {
-        from: 'tiwarisneha491@gmail.com',
-        to: 'tiwarisneha491@gmail.com',
-        subject: 'New Contact Form Submission',
-        text: `Name: ${name}\nTime: ${time}\nPhone: ${phone}\nEmail: ${email}\nNeeds: ${needs},\n` +
-              `UTM Source: ${utm_source || 'Not provided'}\n` +
-              `UTM Medium: ${utm_medium || 'Not provided'}\n` +
-              `UTM Campaign: ${utm_campaign || 'Not provided'}\n` +
-              `UTM Term: ${utm_term || 'Not provided'}\n` +
-              `UTM Content: ${utm_content || 'Not provided'}`,
-};
-
-try {
-    await transporter.sendMail(mailOptions);
-    res.status(200).send('Email sent successfully');
-} catch (error) {
-    console.error('Error sending email', error);
-    res.status(500).send('Error sending email');
-}
-});
 app.post('/api/book', async (req, res) => {
     const { fullName, mobileNumber, email, msg } = req.body;
     const { utm_source, utm_medium, utm_campaign, utm_term, utm_content } = req;
@@ -68,14 +37,14 @@ app.post('/api/book', async (req, res) => {
     const transporter = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
-            user: 'tiwarisneha491@gmail.com',
-            pass: 'qwpx wgzt yhok gqbq',
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
         },
     });
 
     const mailOptions = {
-        from: 'tiwarisneha491@gmail.com',
-        to: 'tiwarisneha491@gmail.com',
+        from: 'process.env.EMAIL_USER',
+        to: 'process.env.EMAIL_USER',
         subject: 'New Appointment Form Submission',
         text: `Full Name: ${fullName}\nMobile Number: ${mobileNumber}\nEmail: ${email}\nmsg: ${msg}\n\n` +
         `UTM Source: ${utm_source || 'Not provided'}\n` +
