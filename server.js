d9443777b618a872564807bde32752bfb2a128c1
sneha-tiwari06 <tiwarisneha491@gmail.com>
2024-09-04 11:39:45 +0530

    changes

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.post('/api/contact', extractUTMParams, async (req, res) => {
     const { utm_source, utm_medium, utm_campaign, utm_term, utm_content } = req;
 
     const transporter = nodemailer.createTransport({
-        service: 'Gmail',
+        service: 'ikagai',
         auth: {
             user: 'devfreelance23m@gmail.com',
             pass: 'crmp helz dgxp qidj',
